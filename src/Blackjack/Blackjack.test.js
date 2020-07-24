5af3568e5db13e6a99c93a619dbfb0a1d140a4a7
Dillon Lee <dillonlee@Dillons-MacBook-Pro.local>
2020-07-24 14:25:31 +0100

    add hand component test

--- src/Blackjack/Blackjack.test.js
+++ src/Blackjack/Blackjack.test.js
@@ -13,28 +13,6 @@ describe('Blackjack', () => {
     it('should display the play button', () => {
       expect(app.getByText('PLAY')).toBeInTheDocument()
     })
-
-    describe('when a user clicks on play', () => {
-      beforeEach(() => {
-        fireEvent.click(app.getByText('PLAY'))
-      })
-      
-      it('should display two cards for the player', () => {
-        const hand = app.getByTestId('player-hand')
-        const cards = hand.querySelectorAll('.card')
-        expect(cards.length).toEqual(2)
-      })
-
-      it('should display one card for the dealer', () => {
-        const hand = app.getByTestId('dealer-hand')
-        const cards = hand.querySelectorAll('.card')
-        expect(cards.length).toEqual(1)
-      })
-
-      it('should display the total points for the player', () => {
-        expect(app.getByText(/Points/)).toHaveTextContent(/Points: \d+/)
-      })
-
-    })
+    
   })
 })
--- src/Blackjack/Hand/Hand.js
+++ src/Blackjack/Hand/Hand.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 function Hand({hand, points, holder, testid}) {
-  const isHolderPlayer = holder === 'Player'
+  const isPlayer = holder === 'Player'
 
   return (
     <div className="hand" data-testid={testid}>
       <h2>{holder}</h2>
         {hand.map(({suit, value}) => (
-          <div key={suit+value} className="card">{suit} {value}</div>
+          <div data-testid="card" key={suit+value} className="card">{suit} {value}</div>
         ))}
-        {isHolderPlayer && <p>Points: {points}</p>}
+        {isPlayer && <p>Points: {points}</p>}
       </div>
   )
 }
